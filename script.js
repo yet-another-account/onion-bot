46b8d60477bf8006ea599e4f4b42851412642578
eukaryote <eukaryote31@gmail.com>
2019-04-24 17:36:12 -0600

    add soft reload

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ jQuery.get('showerthoughts-bot.txt', function(data) {
     var lines = data.split("\n");
     $("#result").html(lines[Math.floor(Math.random() * lines.length)])
 });
+
+function disp() {
+    $("#result").html(lines[Math.floor(Math.random() * lines.length)])
+}
