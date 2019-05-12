6cb1455784669eb7b66035f0076bab5b22797637
eukaryote <eukaryote31@gmail.com>
2019-05-12 13:47:07 -0600

    Change to onion

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var lines;
 
 
-jQuery.get('showerthoughts-bot.txt', function(data) {
+jQuery.get('onion-bot.txt', function(data) {
     lines = data.split("\n");
     $("#result").html(lines[Math.floor(Math.random() * lines.length)])
 });
